54eacd2bf37531803c184314b601c5896125b7a1
Kokodavid <kingkgdave@gmail.com>
2021-04-14 06:32:39 +0300

    Dice Roll

---
+++ app/src/main/java/com/koko/happybirthday/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.koko.happybirthday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+/**
+ * This Activity allows the User to Roll a dice and view
+ * the results on the Screen
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
