6e5c7e94d3031a5b0ee5c56c6481010ba7ca9fbc
Kokodavid <kingkgdave@gmail.com>
2021-04-22 06:11:31 +0300

    Update Teap Calculator UI

---
+++ app/src/main/java/com/koko/happybirthday/TipActivity.kt
@@ -0,0 +1,47 @@
+package com.koko.happybirthday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.koko.happybirthday.databinding.ActivityTipBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
+
+class TipActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTipBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTipBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+
+    }
+
+     private fun calculateTip() {
+         val stringInTextField = binding.costOfServiceEditText.text.toString()
+         val cost = stringInTextField.toDoubleOrNull()
+         if(cost == null){
+             binding.tipResult.text = ""
+             return
+         }
+
+         val tipPercentage = when(binding.tipOptions.checkedRadioButtonId){
+             R.id.option_twenty_percent -> 0.20
+             R.id.option_eighteen_percent -> 0.18
+             else -> 0.15
+         }
+
+         var tip = tipPercentage * cost
+
+         if(binding.roundUpSwitch.isChecked){
+             tip = ceil(tip)
+         }
+
+         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+         binding.tipResult.text = getString(R.string.tip_amount,formattedTip)
+
+
+    }
+}
